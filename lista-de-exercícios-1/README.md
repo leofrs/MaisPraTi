d00d67f79306f91bc961565a5a76af528ed8fafd
Leonardo Santos <leofrrodrigues86@gmail.com>
2024-08-13 10:55:14 -0300

    Exercícios 1 e 2 finalizados

---
+++ lista-de-exercícios-1/README.md
@@ -0,0 +1,6 @@
+1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
+   utilizando uma estrutura de controle if.
+
+2. Crie um programa que classifica a idade de uma pessoa em categorias (criança,
+   adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
+   controle if-else.
---
+++ lista-de-exercícios-1/index.js
@@ -0,0 +1,32 @@
+//Exercício 1
+/* const parOuImpar = (numero) => {
+  if (numero <= 0)
+    return console.error(
+      `O número ${numero} não é válido! Por favor insira um número maior que zero`
+    );
+
+  if (numero % 2 === 0) return console.log(`O numero ${numero} é par`);
+  else return console.log(`O numero ${numero} não é par`);
+};
+parOuImpar(0); */
+
+//Exercício 2
+/* const classificaIdade = (idade) => {
+  if (idade < 0 || idade !== Number)
+    return console.log(
+      `A idade ${idade} inserida não é válida! Insira um valor maior ou igual a 0`
+    );
+
+  if (idade >= 0 && idade <= 1) {
+    console.log(`A idade de ${idade} anos enquadra-se como bebê`);
+  } else if (idade > 1 && idade <= 13) {
+    console.log(`A idade de ${idade} anos enquadra-se como criança`);
+  } else if (idade > 13 && idade <= 17) {
+    console.log(`A idade de ${idade} anos enquadra-se como adolescente`);
+  } else if (idade > 17 && idade < 60) {
+    console.log(`A idade de ${idade} anos enquadra-se como adulto`);
+  } else {
+    console.log(`A idade de ${idade} anos enquadra-se como idoso`);
+  }
+};
+classificaIdade("José"); */
