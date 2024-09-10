c135a1f10414fbbdfbd4cfbab38e0d2c538bb7e6
Leonardo Santos <leofrrodrigues86@gmail.com>
2024-09-10 11:01:56 -0300

    Lista de exercicío 2 iniciada e questão de número 1 feita

---
+++ lista-de-exercicios-2/index.js
@@ -0,0 +1,14 @@
+// QUESTÃO 1
+/* const carro = {
+  marca: "Chevrolet",
+  modelo: "Fusca",
+  ano: "1985",
+  cor: "Azul",
+};
+
+const showCarro = () => {
+  for (let car in carro) {
+    console.log(`${car}: ${carro[car]}`);
+  }
+};
+showCarro(); */
