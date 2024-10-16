11026e5f95d7e1c3f8c29e9ace1041c604226fe0
Leonardo Santos <leofrrodrigues86@gmail.com>
2024-10-16 17:19:37 -0300

    atividade 4 iniciada

--- atividade-4/script.js
+++ atividade-4/script.js
@@ -1,10 +1,9 @@
-let show = true;
-const menuContent = document.querySelector(".content");
-const menuToggle = menuContent.querySelector(".menu-toggle");
-
-menuToggle.addEventListener("click", () => {
-  document.body.style.overflow = show ? "hidden" : "initial";
-
-  menuContent.classList.toggle("on", show);
-  show = !show;
-});
+// script.js
+window.onscroll = function () {
+  const menu = document.querySelector(".menu");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    menu.classList.add("flutuante");
+  } else {
+    menu.classList.remove("flutuante");
+  }
+};
